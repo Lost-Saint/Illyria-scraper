d21924f43e35c3d3dfc6582a813e7d80c8af7e2d
guilty <sunymon@protonmail.com>
2024-11-27 16:27:12 -0500

    add format

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'], // Adjust as needed
-    exclude: ['node_modules', 'dist', 'cypress'],    // Exclude non-test files
-  },
+	test: {
+		include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'], // Adjust as needed
+		exclude: ['node_modules', 'dist', 'cypress'] // Exclude non-test files
+	}
 })
