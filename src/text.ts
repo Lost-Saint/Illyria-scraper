f07f3e6b92a53d08f3bc3a2eec5b227816be5077
guilty <sunymon@protonmail.com>
2024-11-27 18:31:52 -0500

    run format

--- src/text.ts
+++ src/text.ts
@@ -1,6 +1,6 @@
-import * as cheerio from 'cheerio';
-import { mapGoogleCode, type LangCode } from './utils/language.js';
-import request, { Endpoint } from './utils/request.js';
+import * as cheerio from 'cheerio'
+import { mapGoogleCode, type LangCode } from './utils/language.js'
+import request, { Endpoint } from './utils/request.js'
 
 /**
  * Retrieves the translation given a pair of languages and a query
@@ -10,29 +10,30 @@ import request, { Endpoint } from './utils/request.js';
  * @returns A single {@link string} with the translated text
  */
 export const getTranslationText = async (
-    source: LangCode<"source">,
-    target: LangCode<"target">,
-    query: string
+	source: LangCode<'source'>,
+	target: LangCode<'target'>,
+	query: string
 ): Promise<string | null> => {
-    const parsedSource = mapGoogleCode(source);
-    const parsedTarget = mapGoogleCode(target);
+	const parsedSource = mapGoogleCode(source)
+	const parsedTarget = mapGoogleCode(target)
 
-    const encodedQuery = encodeURIComponent(query);
+	const encodedQuery = encodeURIComponent(query)
 
-    if (encodedQuery.length > 7500)
-        return null;
+	if (encodedQuery.length > 7500) {
+		return null
+	}
 
-    return request(Endpoint.TEXT)
-        .with({ source: parsedSource, target: parsedTarget, query: encodedQuery })
-        .doing(({ data }) => {
-            if (!data)
-                return;
+	return request(Endpoint.TEXT)
+		.with({ source: parsedSource, target: parsedTarget, query: encodedQuery })
+		.doing(({ data }) => {
+			if (!data) {
+				return
+			}
 
-			const $ = cheerio.load(data);
-			const translation = $('.result-container').text()?.trim();
-			return translation && !translation.includes('#af-error-page') 
-				? translation 
-				: null;			
-        });
-};
-
+			const $ = cheerio.load(data)
+			const translation = $('.result-container').text()?.trim()
+			return translation && !translation.includes('#af-error-page')
+				? translation
+				: null
+		})
+}
